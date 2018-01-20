7008f6d11246b9eef08a369f0d1f9f3909002d50
Administrator <1792207542@qq.com>
2018-01-20 18:33:49 +0800

    新建项目

--- smile-web/src/main/java/com/smile/core/configuration/SmileWebAppConfigurer.java
+++ smile-web/src/main/java/com/smile/core/configuration/SmileWebAppConfigurer.java
@@ -13,28 +13,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.util.List;
 
-@Configuration
-public class SmileWebAppConfigurer
-        extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        // 多个拦截器组成一个拦截器链
-        // addPathPatterns 用于添加拦截规则
-        // excludePathPatterns 用户排除拦截
-        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns("/login").excludePathPatterns("/logout");
-        super.addInterceptors(registry);
-    }
-
-    @Bean
-    public SmileErrorController basicErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties, List<ErrorViewResolver>errorViewResolvers) {
-        return new SmileErrorController(errorAttributes,serverProperties.getError() ,errorViewResolvers);
-    }
-
-
-    @Bean
-    public SmileErrorAttributes errorAttributes() {
-        return new SmileErrorAttributes();
-    }
-
-}
+//@Configuration
+//public class SmileWebAppConfigurer
+//        extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        // 多个拦截器组成一个拦截器链
+//        // addPathPatterns 用于添加拦截规则
+//        // excludePathPatterns 用户排除拦截
+//        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns("/login").excludePathPatterns("/logout");
+//        super.addInterceptors(registry);
+//    }
+//
+//    @Bean
+//    public SmileErrorController basicErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties, List<ErrorViewResolver>errorViewResolvers) {
+//        return new SmileErrorController(errorAttributes,serverProperties.getError() ,errorViewResolvers);
+//    }
+//
+//
+//    @Bean
+//    public SmileErrorAttributes errorAttributes() {
+//        return new SmileErrorAttributes();
+//    }
+//
+//}
